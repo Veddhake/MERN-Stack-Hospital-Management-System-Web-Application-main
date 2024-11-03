2e5abb917479f448238342ab974092b77bda569a
Veddhake <ved.dhake@gmail.com>
2024-11-03 22:53:19 +0530

    done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -41,10 +41,8 @@ const Navbar = () => {
             <Link to={"/"} onClick={() => setShow(!show)}>
               Home
             </Link>
-            {
-              toast.error("DDDDDDDD");
-              toast.error(isAuthenticated);
-            }
+            {toast.error("DDDDDDDD")}
+            {toast.error(isAuthenticated)}
             {isAuthenticated ? (
               <Link to={"/appointment"} onClick={() => setShow(!show)}>
                 Appointment
