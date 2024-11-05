d30ca1555244d8c4ef2a359082b86ee0a8f98ef1
Veddhake <ved.dhake@gmail.com>
2024-11-05 14:04:32 +0530

    d

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -17,15 +17,26 @@ import {
 
 const router = express.Router();
 
+// router.post("/patient/register", patientRegister);
+// router.post("/login", login);
+// router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
+// router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
+// router.post("/doctor/remove", isAdminAuthenticated, removeDoctor);
+// router.get("/doctors", getAllDoctors);
+// router.get("/patient/me", isPatientAuthenticated, getUserDetails);
+// router.get("/admin/me", isAdminAuthenticated, getUserDetails);
+// router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
+// router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
+
 router.post("/patient/register", patientRegister);
 router.post("/login", login);
-router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
-router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
-router.post("/doctor/remove", isAdminAuthenticated, removeDoctor);
+router.post("/admin/addnew", addNewAdmin);
+router.post("/doctor/addnew", addNewDoctor);
+router.post("/doctor/remove", removeDoctor);
 router.get("/doctors", getAllDoctors);
-router.get("/patient/me", isPatientAuthenticated, getUserDetails);
-router.get("/admin/me", isAdminAuthenticated, getUserDetails);
-router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
-router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
+router.get("/patient/me", getUserDetails);
+router.get("/admin/me", getUserDetails);
+router.get("/patient/logout", logoutPatient);
+router.get("/admin/logout", logoutAdmin);
 
 export default router;
